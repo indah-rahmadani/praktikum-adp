6248b807657957065cfa90166ccaa9fff52d1b46
indah-rahmadani <163073541+indah-rahmadani@users.noreply.github.com>
2024-03-12 07:51:47 +0700

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,20 @@
+print('TUGAS MODUL 1 SHIFT 3')
+print("menghitung volume balok dan luas permukaan kubus")
+print("------------------------------------------------")
+
+print('balok')
+print('-----')
+p=float(input('panjang balok='))
+l=float(input('lebar balok='))
+t=float(input('tinggi balok='))
+print('kubus')
+print('-----')
+s=float(input('sisi kubus='))
+print()
+print('jadi')
+volume_balok=p*l*t
+print(' volume balok adalah',round(volume_balok))
+print('sedangkan')
+volume_kubus=6*s*2
+print('luas permukaan kubus adalah',round(volume_kubus))
+
