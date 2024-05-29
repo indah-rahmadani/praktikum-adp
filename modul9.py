f82505ea002accb74a96d0bfd989d88e76d347b2
indah-rahmadani <163073541+indah-rahmadani@users.noreply.github.com>
2024-05-29 08:57:27 +0700

    Add files via upload

---
+++ modul9.py
@@ -0,0 +1,44 @@
+from termcolor import colored,cprint
+import os
+os.system("cls")
+
+for i in range(4):
+    cprint(" "*3,"red","on_red",end=" ")
+    print(" "*1, end=" ")
+cprint(" "*9,"red","on_red",end=" ")
+print(colored("MASSACHUSETTS", "red"))
+
+
+for i in range(2):
+    cprint(" "*3,"red","on_red",end=" ")
+    print(" "*1, end=" ")
+cprint(" "*3,"red","on_red")
+
+for i in range(3):
+    cprint(" "*3,"red","on_red",end=" ")
+    print(" "*1, end=" ")
+cprint(" "*3,"red","on_dark_grey",end=" ")
+print(" "*1, end=" ")
+cprint(" "*3,"red","on_red",end=" ")
+for i in range (3):
+    print(" "*1, end=" ")
+print(colored("INSTITUTE OF", "red"))
+
+for i in range(3):
+    cprint(" "*3,"red","on_red",end=" ")
+    print(" "*1, end=" ")
+cprint(" "*3,"red","on_dark_grey",end=" ")
+print(" "*1, end=" ")
+cprint(" "*3,"red","on_red")
+
+cprint(" "*3,"red","on_red",end=" ")
+for i in range(4):
+    print(" "*1, end=" ")
+cprint(" "*3,"red","on_red",end=" ")
+print(" "*1, end=" ")
+cprint(" "*3,"red","on_dark_grey",end=" ")
+print(" "*1, end=" ")
+cprint(" "*3,"red","on_red",end=" ")
+for i in range(2):
+    print(" "*2, end=" ")
+print(colored("TECHNOLOGY", "red"))
